81605c64ae12df732417bb8c524e02c5c7c1b74d
tianjiashuo <249672218@qq.com>
2020-05-10 14:07:03 +0800

    init

---
+++ src/main/java/com/workAttendence/workAttendence/WorkAttendenceApplication.java
@@ -0,0 +1,13 @@
+package com.workAttendence.workAttendence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAttendenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkAttendenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workAttendence/workAttendence/WorkAttendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workAttendence.workAttendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkAttendenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
